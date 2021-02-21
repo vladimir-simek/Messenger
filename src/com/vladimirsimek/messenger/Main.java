ba4386d4b7ab84869c2472ac933cad0736044d84
vladimir-simek <minervladik@gmail.com>
2021-02-21 13:06:18 +0100

    cleaned shit in code again

--- src/com/vladimirsimek/messenger/Main.java
+++ src/com/vladimirsimek/messenger/Main.java
@@ -1,9 +1,5 @@
 package com.vladimirsimek.messenger;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
 import static com.vladimirsimek.messenger.Data.*;
 import static com.vladimirsimek.messenger.Logic.*;
 import static com.vladimirsimek.messenger.StealUserData.stealUserData;
