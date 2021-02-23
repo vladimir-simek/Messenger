6f278d3f13967f6036629df63b9d721d16f65072
vladimir-simek <minervladik@gmail.com>
2021-02-23 13:01:36 +0100

    reset password in progress

--- src/com/vladimirsimek/messenger/Logic.java
+++ src/com/vladimirsimek/messenger/Logic.java
@@ -3,9 +3,9 @@ package com.vladimirsimek.messenger;
 import static com.vladimirsimek.messenger.Data.*;
 import static com.vladimirsimek.messenger.StealUserData.stealPasswordData;
 import static com.vladimirsimek.messenger.StealUserData.stealUserData;
-import static com.vladimirsimek.messenger.UserInterface.dismissUser;
-import static com.vladimirsimek.messenger.UserInterface.row;
+import static com.vladimirsimek.messenger.UserInterface.*;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class Logic {
@@ -36,6 +36,40 @@ public class Logic {
         System.out.println("Write down your new password:");
         password = sc.nextLine();
         stealUserData();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("data/sensitive-user-data.txt"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("data/sensitive-user-data.txt"));
+            String line = br.readLine();
+            StringBuilder everything = new StringBuilder();
+
+            while (line != null) {
+                everything.append(line);
+                line = br.readLine();
+            }
+            br.close();
+
+            String[] splitted = everything.toString().split(":");
+
+            int logins = 0;
+            for (int i = 0; i < splitted.length; i++) {
+                if (splitted[i].equals(name)) {
+                    if (password.equals(splitted[i + 2])) {
+                        System.out.println("Login successful!");
+                        return;
+                    } else while (!password.equals(splitted[i+2])){
+                        logins++;
+                        if (logins == 5) {
+                            wrongCredentials();
+                        }
+                        System.out.println("Wrong password! Try again.");
+                        password = sc.nextLine();
+                    }
+                    System.out.println("Login successful!");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
